14d5fc071174244911595a5ac1267100cbc185c2
sealmindset <sealmindset@gmail.com>
2019-08-09 13:34:11 -0500

    Update ADStaticIP.ps1

--- ADStaticIP.ps1
+++ ADStaticIP.ps1
@@ -1,1 +1,3 @@
-New-NetIPAddress –IPAddress 10.211.55.16 -DefaultGateway 10.211.55.1 -PrefixLength 24 -InterfaceIndex (Get-NetAdapter).InterfaceIndex
+New-NetIPAddress –IPAddress 10.211.55.17 -DefaultGateway 10.211.55.1 -PrefixLength 24 -InterfaceIndex (Get-NetAdapter).InterfaceIndex
+
+Set-DNSClientServerAdress –InterfaceIndex (Get-NetAdapter).InterfaceIndex –ServerAddresses 10.211.55.16
