68fb8cd3c85d48bbcf483838ee46ade63c694dd6
sealmindset <sealmindset@gmail.com>
2019-08-09 14:01:52 -0500

    Update ADSetup.ps1

--- ADSetup.ps1
+++ ADSetup.ps1
@@ -1,16 +1,16 @@
-$PSVersionTable.PSVersion
+# $PSVersionTable.PSVersion
 
 # Check if AD Services is installed
-#Add-WindowsFeature AD-Domain-Services
-#Add-windowsfeature RSAT-ADDS
-
-Import-Module ADDSDeployment
+# Add-WindowsFeature AD-Domain-Services
+# Add-windowsfeature RSAT-ADDS
 
 # Check if Administator password is not null
-#net user Administrator P@ssw0rd2013
-#net user Administrator /passwordreq:yes
+# net user Administrator P@ssw0rd2013
+# net user Administrator /passwordreq:yes
+
+Import-Module ADDSDeployment
 
 $SafeModeAdministratorPasswordText = ‘P@ssw0rd2013’
 $SafeModeAdministratorPassword = ConvertTo-SecureString -AsPlainText $SafeModeAdministratorPasswordText -Force
 
-Install-ADDSForest -CreateDNSDelegation:$False -DatabasePath “c:\Windows\NTDS” -DomainMode ‘Win2012R2’ -DomainName "tst.com” -DomainNetbiosName “TSTAD” -ForestMode ‘Win2012R2’ -InstallDNS:$true -LogPath “C:\Windows\NTDS” -NoRebootOnCompletion:$false -Sysvolpath “C:\Windows\SYSVOL” -Force:$true -SafeModeAdministratorPassword $SafeModeAdministratorPassword
+Install-ADDSForest -CreateDNSDelegation:$False -DatabasePath “c:\Windows\NTDS” -DomainMode ‘Win2012R2’ -DomainName "tst.com” -DomainNetbiosName “W12SrvrAD” -ForestMode ‘Win2012R2’ -InstallDNS:$true -LogPath “C:\Windows\NTDS” -NoRebootOnCompletion:$false -Sysvolpath “C:\Windows\SYSVOL” -Force:$true -SafeModeAdministratorPassword $SafeModeAdministratorPassword
